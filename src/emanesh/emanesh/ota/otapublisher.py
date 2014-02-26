bd32edea797369a2d8a91b1c1a9689707b0b74e1
Steven Galgano <sgalgano@adjacentlink.com>
2014-02-26 15:02:08 -0500

    Fixed get_ip_address issue when specifying an interface

--- src/emanesh/emanesh/ota/otapublisher.py
+++ src/emanesh/emanesh/ota/otapublisher.py
@@ -37,6 +37,16 @@ import socket
 import fcntl
 import uuid
 import threading
+import struct
+
+def get_ip_address(ifname):
+    # http://code.activestate.com/recipes/439094/
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 def init_multicast_socket(group,port,device):
     try:
